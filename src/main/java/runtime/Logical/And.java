a8a720d1c92b58ce3dec1faae7f8786929f3100b
Raghav1794 <35378167+Raghav1794@users.noreply.github.com>
2018-05-04 06:01:40 -0700

    Update And.java

--- src/main/java/runtime/Logical/And.java
+++ src/main/java/runtime/Logical/And.java
@@ -1,8 +1,5 @@
 package runtime.Logical;
 
-/**
- * Created by dixith on 4/23/17.
- */
 public class And extends Logical {
     public And(Boolean operand1, Boolean operand2) {
         super(operand1, operand2);
