b4ebc030914f9c6998cbe55b9ee569e6042a97fd
Raghav1794 <35378167+Raghav1794@users.noreply.github.com>
2018-05-04 06:04:30 -0700

    Update Lesser.java

--- src/main/java/runtime/conditional/Lesser.java
+++ src/main/java/runtime/conditional/Lesser.java
@@ -1,8 +1,5 @@
 package runtime.conditional;
 
-/**
- * Created by dixith on 4/23/17.
- */
 public class Lesser extends Conditional {
     public Lesser(int operand1, int operand2) {
         super(operand1, operand2);
