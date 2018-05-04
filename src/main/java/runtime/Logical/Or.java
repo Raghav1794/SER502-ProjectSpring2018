218e590fdf5e2e824c5ef2aa3a6e669c1cbc9d93
Raghav1794 <35378167+Raghav1794@users.noreply.github.com>
2018-05-04 06:03:08 -0700

    Update Or.java

--- src/main/java/runtime/Logical/Or.java
+++ src/main/java/runtime/Logical/Or.java
@@ -1,8 +1,5 @@
 package runtime.Logical;
 
-/**
- * Created by dixith on 4/23/17.
- */
 public class Or extends Logical {
     public Or(Boolean operand1, Boolean operand2) {
         super(operand1, operand2);
